4f9215e89d43ed13ab1b141f643b505570bc86e1
Cole McDougal <xmrmcdougal@gmail.com>
2025-06-18 16:47:22 -0700

    connected to GitLab and created working branch

---
+++ src/main/java/com/d288PA/WGU/D288PerformanceAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.d288PA.WGU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D288PerformanceAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D288PerformanceAssessmentApplication.class, args);
+	}
+
+}
